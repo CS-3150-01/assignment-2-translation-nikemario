270ab664a27e2e65bbecd073ba0ff2e4b21e5f10
Hannah Small <hannah.small01@gmail.com>
2022-10-02 13:20:49 -0600

    bst translation done

--- C/BinarySearchTree.c
+++ C/BinarySearchTree.c
@@ -1,5 +1,60 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    return 0;
+int main(void) {
+	
+	struct Node *root = NULL;
+	
+	printf("Total number of possible Binary Search Trees with given key: %d", numOfBST(5));
+	
+	return 0;
+	
+}
+
+struct Node {
+	
+	int nodeData;
+	struct Node *left;
+	struct Node *right;
+	
+};
+
+struct Node *newNode(int data) {
+	
+	struct Node *temp = (struct Node *)malloc(sizeof(struct Node));
+	temp -> nodeData = data;
+	temp -> left = NULL;
+	temp -> right = NULL;
+	
+	return temp;
+	
+}
+
+int fact(int num) {
+	
+	int fact = 1;
+	
+	if (num == 0) {
+		return 1;
+	}
+	else {
+		
+		while (num > 1) {
+			
+			fact = fact * num;
+			num--;
+			
+		}
+		
+		return fact;
+		
+	}
+	
+}
+
+int numOfBST(int key) {
+	
+	int catalanNumber = fact(2 * key)/(fact(key + 1) * fact(key));
+	return catalanNumber;
+	
 }
